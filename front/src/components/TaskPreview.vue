df743d5b1aea0de2e179e6a9ad997ecc1975e02f
unknown <Nevonoam@gmail.com>
2019-11-27 16:36:51 +0200

    route

--- front/src/components/TaskPreview.vue
+++ front/src/components/TaskPreview.vue
@@ -2,5 +2,22 @@
   <section>
     <h1>Task preview</h1>
     <pre>name:{{task.name}}</pre>
+    <router-link :to="taskEditLink"></router-link>
   </section>
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params._id
+    };
+  },
+  computed: {
+    taskEditLink() {
+      return `/task/edit/${this.id}`;
+    }
+  }
+};
+</script>
