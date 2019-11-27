1b16b4ac795120c8bd4c9c8a7407211f98715ff3
unknown <Nevonoam@gmail.com>
2019-11-27 19:29:28 +0200

    jjk

--- front/src/services/BoardService.js
+++ front/src/services/BoardService.js
@@ -1,12 +1,41 @@
-import HttpService from './HttpService.js'
+// import axios from 'axios'
+// axios.defaults.withCredentials=true
 
 export default {
     query,
     add,
     remove,
+    getById,
     edit
 }
 
-function query(filterBy = {}) {
+const BASE_URL = (process.env.NODE_ENV !== 'development') ?
+    '/api/toy' :
+    '//localhost:3000/boards';
 
+
+function query() {
+    return axios.get(BASE_URL).then(res => {
+        return res.data;
+    });
+}
+
+function add(toy) {
+    return axios.post(BASE_URL, toy)
+        .then(res => res.data)
+}
+
+function remove(todoId) {
+    return axios.delete(`${BASE_URL}/${todoId}`)
+}
+
+function getById(id) {
+    return axios.get(`${BASE_URL}/${id}`)
+        .then(toys => {
+            return toys.data
+        })
+}
+
+function edit(toyToEdit) {
+    return axios.put(`${BASE_URL}/${toyToEdit.toy.id}`, toyToEdit.toy)
 }
