7079e92e90492866262b56e02482e908c9857c35
unknown <Nevonoam@gmail.com>
2019-11-27 18:10:44 +0200

    jjdjk

--- front/src/components/TopicsPreview.vue
+++ front/src/components/TopicsPreview.vue
@@ -1,20 +1,74 @@
 <template>
   <section>
-    <li>
-      {{topic}}
-      <button @click="edit">Edit</button>
-    </li>
+    <div class="row">
+      <div class="col-3">
+        <h3>{{topic}}</h3>
+        <draggable
+          class="dragArea list-group"
+          :list="list1"
+          :clone="clone"
+          :group="{ name: 'people', pull: pullFunction }"
+          @start="start"
+        >
+          <div class="list-group-item" v-for="element in list1" :key="element.id">{{ element.name }}</div>
+        </draggable>
+      </div>
+
+      <div class="col-3">
+        <h3>Draggable 2</h3>
+        <draggable class="dragArea list-group" :list="list2" group="people">
+          <div class="list-group-item" v-for="element in list2" :key="element.id">{{ element.name }}</div>
+        </draggable>
+      </div>
+
+      <rawDisplayer class="col-3" :value="list1" title="List 1" />
+
+      <rawDisplayer class="col-3" :value="list2" title="List 2" />
+    </div>
   </section>
 </template>
 
 <script>
+import draggable from "vuedraggable";
+import rawDisplayer from "vuedraggable";
+
+let idGlobal = 8;
 export default {
   props: {
     topic: Object
   },
+  name: "clone-on-control",
+  display: "Clone on Control",
+  instruction: "Press Ctrl to clone element from list 1",
+  order: 4,
+  components: {
+    draggable,
+    rawDisplayer
+  },
+  data() {
+    return {
+      list1: [
+        { name: "Jesus", id: 1 },
+        { name: "Paul", id: 2 },
+        { name: "Peter", id: 3 }
+      ],
+      list2: [
+        { name: "Luc", id: 5 },
+        { name: "Thomas", id: 6 },
+        { name: "John", id: 7 }
+      ],
+      controlOnStart: true
+    };
+  },
   methods: {
-    edit() {
-      console.log(this.topic);
+    clone({ name }) {
+      return { name, id: idGlobal++ };
+    },
+    pullFunction() {
+      return this.controlOnStart ? "clone" : true;
+    },
+    start({ originalEvent }) {
+      this.controlOnStart = originalEvent.ctrlKey;
     }
   }
 };
