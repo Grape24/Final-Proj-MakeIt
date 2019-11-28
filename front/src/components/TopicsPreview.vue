3579219eb7baf6491645afe1784b7c507896bc0e
unknown <Nevonoam@gmail.com>
2019-11-28 09:18:26 +0200

    test

--- front/src/components/TopicsPreview.vue
+++ front/src/components/TopicsPreview.vue
@@ -14,12 +14,7 @@
         </draggable>
       </div>
 
-      <div class="col-3">
-        <h3>Draggable 2</h3>
-        <draggable class="dragArea list-group" :list="list2" group="people">
-          <div class="list-group-item" v-for="element in list2" :key="element.id">{{ element.name }}</div>
-        </draggable>
-      </div>
+     
 
       <rawDisplayer class="col-3" :value="list1" title="List 1" />
 
