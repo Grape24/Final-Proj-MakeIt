ebdc885513dcd8aed1d49f3d8d33cbbbbc2262a1
adipinhas <adi9247@gmail.com>
2019-11-28 08:39:02 +0200

    service

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,14 +1,50 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import BoardService from '../services/BoardService'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    boards:[],
+    currBoard:null
+  },
+  getters:{
+    boards(state){
+      return state.boards
+    },
+    currBoard(state){
+      return state.currBoard
+    }
   },
   mutations: {
+    setCurrBoard(state, { board }) {
+      state.currboard = board;
+  },
+  setBoards(state, { boards }) {
+
+    state.boards = boards;
+},
+
+  
+  addBoard(state, {board}) {
+    const addedBoard = BoardService.add(board)
+    state.boards.push(addedBoard)
+  },
+ 
+
+
+
   },
   actions: {
+    loadBoards(context) {
+      return BoardService.query()
+          .then(boards => {
+              context.commit({type: 'setBoards', boards})
+              return boards;
+          })
+  },
+
   },
   modules: {
   }
