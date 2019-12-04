959adfc346f39816feac2748b55a0608cae9a828
unknown <Nevonoam@gmail.com>
2019-12-04 16:58:39 +0200

    x

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,17 +1,35 @@
 <template>
-  <div class="header flex space-between align-center">
-    <router-link class="home-icon-container" to="/"><i class="fas fa-home"></i></router-link>
-    <div class="logo"><i class="fas fa-check-double"></i>MakeIt</div>
-    <div class="nav-links-container flex ">
-      <router-link class="flex" to="/">logout</router-link>
-      <router-link class="flex" to="/login">login</router-link>
-      <router-link class="flex" to="/singup">singup</router-link>
+  <section>
+    <div class="header flex space-between align-center">
+      <router-link class="home-icon-container" to="/">
+        <i class="fas fa-home"></i>
+      </router-link>
+      <div class="logo">
+        <i class="fas fa-check-double"></i>MakeIt
+      </div>
+      <input type="text" placeholder="search" @input="setFilter" v-model="filterTxt" />
+      {{filterTxt}}
+      <div class="nav-links-container flex">
+        <router-link class="flex" to="/">logout</router-link>
+        <router-link class="flex" to="/login">login</router-link>
+        <router-link class="flex" to="/singup">singup</router-link>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
+  data() {
+    return {
+      filterTxt: ""
+    };
+  },
+  methods:{
+    setFilter(){
+     console.log('ss')
+    }
+  }
 };
 </script>
