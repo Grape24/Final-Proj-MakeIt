1e4d84d665558c144b8112bff54ba0e71bf45c34
Enav <eavs772@gmail.com>
2019-12-05 09:10:46 +0200

    ahhhhhhhh

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="header flex space-between align-center">
+  <div class="header flex align-center">
     <router-link class="home-icon-container" to="/"><i class="fas fa-home"></i></router-link>
     <div class="logo"><i class="fas fa-check-double"></i>MakeIt</div>
-    <div class="nav-links-container flex ">
+    <div v-if="false" class="nav-links-container flex ">
       <router-link class="flex" to="/">logout</router-link>
       <router-link class="flex" to="/login">login</router-link>
       <router-link class="flex" to="/singup">singup</router-link>
